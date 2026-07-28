d63f3c6cc4e781d67797166849199f98d16b900a
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-07-28 01:29:34 -0400

    v6.4 (2f/4): Felicia sprite payload parts 11-12 + assembler

---
+++ felicia.js
@@ -0,0 +1,3 @@
+// v6.4 Felicia sprite assembly: 768x128 combo (0-511 walk atlas 4x128, 512-639 portrait, 640-767 Impreza)
+const TO_FELICIA = "data:image/png;base64," + FEL_B64_1 + FEL_B64_2 + FEL_B64_3 + FEL_B64_4 + FEL_B64_5 + FEL_B64_6 + FEL_B64_7 + FEL_B64_8 + FEL_B64_9 + FEL_B64_10 + FEL_B64_11 + FEL_B64_12;
+const FEL_ATLAS = { cell: 128, frames: { down0: [0,0], right0: [1,0], up0: [2,0], violin: [3,0] }, portrait: [4,0], car: [5,0] };
