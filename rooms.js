53d68de9807c7e58010be19187a51c12d11cd704
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-07-28 18:07:14 -0400

    v7.0: index.html (8 IT chunks + v70 tag) and rooms.js (8-part assembly)

--- rooms.js
+++ rooms.js
@@ -1,6 +1,6 @@
 // v6.9 side-view department backdrops (480x270, rendered fullscreen)
 const TO_ROOMS = {
-  itdept: "data:image/jpeg;base64," + ROOM_B64_IT_1 + ROOM_B64_IT_2 + ROOM_B64_IT_3 + ROOM_B64_IT_4,
+  itdept: "data:image/jpeg;base64," + ROOM_B64_IT_1 + ROOM_B64_IT_2 + ROOM_B64_IT_3 + ROOM_B64_IT_4 + ROOM_B64_IT_5 + ROOM_B64_IT_6 + ROOM_B64_IT_7 + ROOM_B64_IT_8,
   eng: "data:image/jpeg;base64," + ROOM_B64_ENG_1 + ROOM_B64_ENG_2 + ROOM_B64_ENG_3,
   factory: "data:image/jpeg;base64," + ROOM_B64_FAC_1 + ROOM_B64_FAC_2 + ROOM_B64_FAC_3,
   office: "data:image/jpeg;base64," + ROOM_B64_OFC_1 + ROOM_B64_OFC_2 + ROOM_B64_OFC_3,
