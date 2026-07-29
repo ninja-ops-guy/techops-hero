707e5ef3a32ab35fec4368ff8680ff3141406187
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-07-28 22:40:36 -0400

    v7.3: story panel assembler

---
+++ panels.js
@@ -0,0 +1,9 @@
+// v7.3 "The Woman on the Wing": generated story-panel art (chibi pixel-comic style)
+const TO_PANELS = [
+  "data:image/jpeg;base64," + TO_P1A + TO_P1B,
+  "data:image/jpeg;base64," + TO_P2A + TO_P2B,
+  "data:image/jpeg;base64," + TO_P3A + TO_P3B + TO_P3C,
+  "data:image/jpeg;base64," + TO_P4A + TO_P4B + TO_P4C + TO_P4D + TO_P4E,
+  "data:image/jpeg;base64," + TO_P5A + TO_P5B + TO_P5C,
+  "data:image/jpeg;base64," + TO_P6A + TO_P6B + TO_P6C + TO_P6D,
+];
