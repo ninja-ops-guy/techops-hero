3704255adbe69cf8e555150fb7a7e01fa6c2ac8d
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-08-24 23:32:34 -0400

    Test Day 1 cinematic motion profiles

--- test_campaign_native_act1_visuals.js
+++ test_campaign_native_act1_visuals.js
@@ -7,6 +7,7 @@ global.closeDlg = function () { return true; };
 
 const visuals = require("./campaign_native_act1_visuals.js");
 
+assert.strictEqual(visuals.VERSION, 2);
 assert.strictEqual(visuals.url("shipping.dock_background"), "assets/campaign/shipping.dock_background.png");
 assert.strictEqual(visuals.sceneForDialog("CAMPAIGN STANDUP"), "standup");
 assert.strictEqual(visuals.sceneForDialog("WORKSTATION // COMPANY"), "workstation");
@@ -20,5 +21,10 @@ assert.strictEqual(visuals.SCENES.shipping.background, "shipping.dock_background
 assert.strictEqual(visuals.SCENES.shipping.actor, "shipping.clerk.idle");
 assert.deepStrictEqual(visuals.SCENES.plating.props, ["plating.workstation_cracked", "plating.line_stopped_display"]);
 assert.deepStrictEqual(visuals.SCENES.workstation.props, ["workstation.felicia.video_frame", "workstation.orpheus.glitch_frame"]);
+assert.deepStrictEqual(visuals.motionFor("shipping"), ["forklift_pass", "printer_feed", "camera_track"]);
+assert.deepStrictEqual(visuals.motionFor("plating"), ["warning_beacon", "machine_idle", "camera_track"]);
+assert.deepStrictEqual(visuals.motionFor("workstation"), ["screen_scan", "orpheus_glitch", "camera_push"]);
+assert.deepStrictEqual(visuals.motionFor("access"), ["audit_sweep", "evidence_pulse", "camera_push"]);
+assert.deepStrictEqual(visuals.motionFor("missing"), []);
 
-console.log("Campaign Day 1 reference visuals: PASS");
+console.log("Campaign Day 1 reference visuals + cinematic motion: PASS");
