8abd7c548bd60d097dcdac6c2d30c4703c6000fb
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-12 13:38:35 -0400

    Add EAST SIDE to production release gate

--- scripts/production_release_gate.js
+++ scripts/production_release_gate.js
@@ -15,6 +15,7 @@ const tests = [
   "test_good_dogs_route_contract.js",
   "test_good_dogs_coop.js",
   "test_good_dogs_state_integrity.js",
+  "test_east_side.js",
   "test_campaign_world_visuals.js",
   "test_campaign_late_game_contracts.js",
   "test_good_boys_canon_runtime.js",
